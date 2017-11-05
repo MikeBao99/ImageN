8eefc006a9075507a8cefb9784e9e6f90011feff
Mike Bao <mikebao@college.harvard.edu>
2017-11-05 18:16:46 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,7 +4,10 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def homepage():
-    return render_template('homepage.html')
+    if request.method == "POST":
+        return render_template('homepage.html', WordCount = "We Win!!")
+    else:
+        return render_template('homepage.html', WordCount = "Did we win yet?")
 
 @views.route('/about')
 def about():
