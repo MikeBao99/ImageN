b97cb00f4edcdcc58ad0c65756e939ab06e2274c
Mike Bao <mikebao@college.harvard.edu>
2017-11-05 19:28:03 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -13,19 +13,8 @@ def homepage():
     if request.method == "POST":
         if not request.form.get("input-b1"):
             return render_template('homepage.html', WordCount = "No File Found")
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['input-b1']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            return render_template('homepage.html', WordCount = "File Received!")
-        return render_template('homepage.html', WordCount = "We Win!!")
+        return render_template('homepage.html', WordCount = "File Received!")
+        #return render_template('homepage.html', WordCount = "We Win!!")
     else:
         return render_template('homepage.html', WordCount = "Did we win yet?")
 
