bb033a3484aa71839542fd381882705856084e40
Mike Bao <mikebao@college.harvard.edu>
2017-11-05 21:23:11 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import * #TODO: actually look at imports
-import ocr
+import imagen-ocr
 
 views = Blueprint('views', __name__)
 
@@ -14,7 +14,7 @@ def homepage():
     if request.method == "POST":
         if not request.form.get("input-b1"):
             return render_template('homepage.html', WordCount = "No File Found")
-        return render_template('homepage.html', WordCount = ocr_space_file(filename = request.form.get("input-b1")))
+        return render_template('homepage.html', WordCount = ocr_space_url(url='http://i.imgur.com/31d5L5y.jpg'))
         #return render_template('homepage.html', WordCount = "We Win!!")
     else:
         return render_template('homepage.html', WordCount = "")
