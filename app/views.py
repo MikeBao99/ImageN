1ef0ec8d16fea1e36b36a2037ff94008861a3adb
Mike Bao <mikebao@college.harvard.edu>
2017-11-08 15:59:07 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import * #TODO: actually look at imports
 import boto3
 import requests
 import json
+import urllib
 from werkzeug import * #TODO: actually look at imports
 
 views = Blueprint('views', __name__)
@@ -15,30 +16,14 @@ def file_allowed(file):
 @views.route('/', methods=["GET", "POST"])
 def homepage():
     if request.method == "POST":
-#         print str(request.files)
-#         if not len(request.files) == 0:
-#             return render_template('homepage.html', WordCount = "No File Found")
-        #ocr-im = ocr_space_url(url='https://s3.us-east-2.amazonaws.com/imagen50/IMG_1732.JPG')
+
         s3 = boto3.resource('s3')
-#         s3.Bucket('imagen50').put_object(Key=request.files['input-b1'].filename, Body=request.files['input-b1'].stream.read())
-        
-#         name = request.files['input-b1'].filename
-#         formatted_name = ''
-#         for let in name:
-#             if let == '.':
-#                 break
-#             else:
-#                 formatted_name = formatted_name + let
+
         bucket = s3.Bucket('imagen50')
         bucket.upload_fileobj(request.files['input-b1'], request.files['input-b1'].filename, ExtraArgs={'ContentType': 'image/jpeg'})
-#         formatted_name = formatted_name + '.JPG'
-        
-#         s3_object = s3.Object('imagen50', request.files['input-b1'].filename)
-#         s3_object.metadata.update({'Content-Type':'image/jpg'})
-#         s3_object.copy_from(CopySource={'Bucket':'imagen50', 'Key':request.files['input-b1'].filename}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
-        
-        return render_template('homepage.html', WordCount =  json.loads(ocr_space_url(url='https://s3.us-east-2.amazonaws.com/imagen50/%s' % request.files['input-b1'].filename))['ParsedText'])
-        #return render_template('homepage.html', WordCount = "We Win!!")
+    
+        link = 'https://s3.us-east-2.amazonaws.com/imagen50/%s' % urllib.quote_plus(request.files['input-b1'].filename)
+        return render_template('homepage.html', WordCount =  json.loads(ocr_space_url(url=link))['ParsedText'])
     else:
         return render_template('homepage.html', WordCount = "")
 
