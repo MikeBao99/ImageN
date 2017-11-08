cdda2cf0562f87afb000912e0b046940b6dc3ded
Mike Bao <mikebao@college.harvard.edu>
2017-11-08 01:06:07 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -20,6 +20,13 @@ def homepage():
         #ocr-im = ocr_space_url(url='https://s3.us-east-2.amazonaws.com/imagen50/IMG_1732.JPG')
         s3 = boto3.resource('s3')
         s3.Bucket('imagen50').put_object(Key=request.files['input-b1'].filename, Body=request.files['input-b1'].stream.read())
+        
+        bucket = 'imagen50'
+        key = request.files['input-b1'].filename
+        k = client.head_object(Bucket = bucket, Key = key)
+        m = k["Metadata"]
+        m["new_metadata"] = "image/jpg"
+        client.copy_object(Bucket = bucket, Key = key, CopySource = bucket + '/' + key, Metadata = m, MetadataDirective='REPLACE')
         return render_template('homepage.html', WordCount =  'https://s3.us-east-2.amazonaws.com/imagen50/%s' % request.files['input-b1'].filename + '\n \n' + str(ocr_space_url(url='https://s3.us-east-2.amazonaws.com/imagen50/%s' % request.files['input-b1'].filename)))
         #return render_template('homepage.html', WordCount = "We Win!!")
     else:
