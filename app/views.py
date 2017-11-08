6bb948d31a1fc207747a3a15e457fe57e09dc0c2
Mike Bao <mikebao@college.harvard.edu>
2017-11-08 01:35:57 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -19,11 +19,22 @@ def homepage():
 #             return render_template('homepage.html', WordCount = "No File Found")
         #ocr-im = ocr_space_url(url='https://s3.us-east-2.amazonaws.com/imagen50/IMG_1732.JPG')
         s3 = boto3.resource('s3')
-        s3.Bucket('imagen50').put_object(Key=request.files['input-b1'].filename, Body=request.files['input-b1'].stream.read())
+#         s3.Bucket('imagen50').put_object(Key=request.files['input-b1'].filename, Body=request.files['input-b1'].stream.read())
         
-        s3_object = s3.Object('imagen50', request.files['input-b1'].filename)
-        s3_object.metadata.update({'Content-Type':'image/jpg'})
-        s3_object.copy_from(CopySource={'Bucket':'imagen50', 'Key':request.files['input-b1'].filename}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
+#         name = request.files['input-b1'].filename
+#         formatted_name = ''
+#         for let in name:
+#             if let == '.':
+#                 break
+#             else:
+#                 formatted_name = formatted_name + let
+        bucket = s3.Bucket('imagen50')
+        bucket.upload_fileobj(request.files['input-b1'].stream.read(), request.files['input-b1'].filename, ExtraArgs={'ContentType': 'image/jpeg'})
+#         formatted_name = formatted_name + '.JPG'
+        
+#         s3_object = s3.Object('imagen50', request.files['input-b1'].filename)
+#         s3_object.metadata.update({'Content-Type':'image/jpg'})
+#         s3_object.copy_from(CopySource={'Bucket':'imagen50', 'Key':request.files['input-b1'].filename}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
         
         return render_template('homepage.html', WordCount =  'https://s3.us-east-2.amazonaws.com/imagen50/%s' % request.files['input-b1'].filename + '\n \n' + str(ocr_space_url(url='https://s3.us-east-2.amazonaws.com/imagen50/%s' % request.files['input-b1'].filename)))
         #return render_template('homepage.html', WordCount = "We Win!!")
